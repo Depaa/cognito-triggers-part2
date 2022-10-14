ea9b80da1918a75eaf14ffe5f473feb2e6f559f2
Matteo Depascale <depascale.matteo@gmail.com>
2022-10-14 18:43:11 +0200

    sbrodolone

--- backend/src/lambdas/trigger-pre-signup/index.js
+++ backend/src/lambdas/trigger-pre-signup/index.js
@@ -1,30 +1,37 @@
 // allow only gmail, icloud and outlook addresses
 // start step function to remind user and to delete unconfirmed user
-// const { StepFunctions } = require('aws-sdk');
+const { StepFunctions } = require('aws-sdk');
 
-// const sfn = new StepFunctions();
+const sfn = new StepFunctions();
 
 exports.handler = async (event) => {
   console.debug(event);
 
-  // const address = event.request.userAttributes.email.split("@");
-  // if (address[1] !== 'gmail.com' && address[1] !== 'icloud.com' && address[1] !== 'outlook.com') {
-  //   throw new Error('You must use gmail, icloud or outlook');
-  // }
+  const address = event.request.userAttributes.email.split("@");
+  if (address[1] !== 'gmail.com' && address[1] !== 'icloud.com' && address[1] !== 'outlook.com') {
+    throw new Error('You must use gmail, icloud or outlook');
+  }
 
-  // const input = {
-  //   id: event.userName,
-  // }
+  const input = {
+    id: event.userName,
+  }
 
-  // await sfn.startExecution({
-  //   input: JSON.stringify(JSON.stringify(input)),
-  //   name: event.userName,
-  //   stateMachineArn: process.env.REMINDER_STATE_MACHINE
-  // }).promise();
-  // console.info(`Successfully start execution for ${event.userName}`);
+  await sfn.startExecution({
+    input: JSON.stringify(JSON.stringify(input)),
+    name: event.userName,
+    stateMachineArn: process.env.REMINDER_STATE_MACHINE
+  }).promise();
+  console.info(`Successfully start execution for ${event.userName}`);
 
+  return event;
+}
+
+/*
+exports.handler = async (event) => {
+  console.debug(event);
   // for face recognition authentication we need to autoconfirm users
   event.response.autoConfirmUser = true;
   event.response.autoVerifyEmail = true;
   return event;
 }
+*/
