402ac1f697534d93d505f09cff1130d9db98b19f
Depaa <depascale.matteo@gmail.com>
2022-11-06 10:53:32 +0100

    fix deprecated issue

--- frontend/src/shared/CameraFunctions.ts
+++ frontend/src/shared/CameraFunctions.ts
@@ -12,7 +12,7 @@ const cameraFunctions = {
       let mime: any;
       const mimeTemp = arr[0].match(/:(.*?);/);
       if (mimeTemp) mime = mimeTemp[1];
-      const bstr = atob(arr[1]);
+      const bstr = window.atob(arr[1]); //Buffer.from(arr[1], 'base64');
       let n = bstr.length,
         u8arr = new Uint8Array(n);
       while (n--) {
@@ -20,6 +20,10 @@ const cameraFunctions = {
       }
       return new File([u8arr], filename, { type: mime });
     }
+  },
+  dataURLtoFile2: async (dataurl: URL, filename: string) => {
+    const blob = await (await fetch(dataurl)).blob();
+    return new File([blob], 'filename', { type: "image/jpeg" });
   },
 };
 
